d3fe1a3a4c64d48de300bbbfabd9519b5a3250be
William Gonzales <wgonz94@yahoo.com>
2019-11-09 13:56:57 -0800

    updated eslint rules

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,14 +13,14 @@ module.exports = function(app) {
     db.User.destroy({ where: { id: req.params.id } }).then(function(dbProj) {
       res.json(dbProj);
     });
-  })
+  });
 
   //update user info
   app.put("/api/user/:id", (req, res) => {
-    db.User.update(req.body,
-      {where: {id: req.params.id}
-    }).then(function(dbProj) {
-      res.json(dbProj)
+    db.User.update(req.body, { where: { id: req.params.id } }).then(function(
+      dbProj
+    ) {
+      res.json(dbProj);
     });
   });
 
@@ -31,39 +31,41 @@ module.exports = function(app) {
     });
   });
 
-    //Get route to view list for projects under user
- app.get("/api/user/:id", (req, res) => {
-  db.Measurement.findAll({
-    where: {
-      foreignKey: req.params.id
-    }
-  }).then(function(dbProj) {
+  //Get route to view list for projects under user
+  app.get("/api/user/:id", (req, res) => {
+    db.Measurement.findAll({
+      where: {
+        foreignKey: req.params.id
+      }
+    }).then(function(dbProj) {
       res.json(dbProj);
+    });
   });
-});
 
-//Get route to view single project
-app.get("/api/projects/:id", (req, res) => {
-  db.Measurement.findOne({
-      where: {id: req.params.id },
-  }).then(function(dbProj) {
-      res.json(dbProj)
+  //Get route to view single project
+  app.get("/api/projects/:id", (req, res) => {
+    db.Measurement.findOne({
+      where: { id: req.params.id }
+    }).then(function(dbProj) {
+      res.json(dbProj);
+    });
   });
-});
 
   // Delete a project by id
   app.delete("/api/projects/:id", (req, res) => {
-    db.Measurement.destroy({ where: { id: req.params.id } }).then(function(dbProj) {
+    db.Measurement.destroy({ where: { id: req.params.id } }).then(function(
+      dbProj
+    ) {
       res.json(dbProj);
     });
   });
 
   // update a project by id
   app.put("/api/projects/:id", (req, res) => {
-    db.Measurement.update(req.body,
-      {where: {id: req.params.id}
-    }).then(function(dbProj) {
-      res.json(dbProj)
-    });
+    db.Measurement.update(req.body, { where: { id: req.params.id } }).then(
+      function(dbProj) {
+        res.json(dbProj);
+      }
+    );
   });
 };
