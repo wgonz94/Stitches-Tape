15d8930a806c37b9486b8f1d4f13def1695434a9
SirHexxus <jamesmichaelstacy@gmail.com>
2019-11-12 11:09:51 -0800

    applied styling generated by Devyn to all pages, and made minor fixes to other files

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $(".sidenav").sidenav();
+  $(".carousel").carousel();
+  $(".parallax").parallax();
+  $(".tabs").tabs();
+});
+
+$(document).ready(() => {
+  $(".user-name").html("<b>Welcome Back User</b>");
+});
