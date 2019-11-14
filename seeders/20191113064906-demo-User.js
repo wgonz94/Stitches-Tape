2c704772a1cb37d3b7574dd26a0851cd6b3a170c
Scottie <avscott@pacbell.net>
2019-11-13 18:35:22 -0800

    fix user seeder file

--- seeders/20191113064906-demo-User.js
+++ seeders/20191113064906-demo-User.js
@@ -1,27 +1,32 @@
 "use strict";
 
+// let Sequelize = require("sequelize");
 const faker = require("faker");
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    let data = [];
+let data = [];
 
-    for (var i = 0; i < 11; i++) {
-      data.push({
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        phoneNumber: faker.phone.phoneNumber(),
-        isDesigner: faker.random.boolean,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      });
-    }
+for (var i = 0; i < 11; i++) {
+  let newUser = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    phoneNumber: faker.phone.phoneNumber(),
+    isDesigner: faker.random.boolean(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  };
+  data.push(newUser);
+}
+
+console.log(data);
+
+module.exports = {
+  up: (queryInterface) => {
     return queryInterface.bulkInsert("Users", data, {});
   },
 
-  down: (queryInterface, Sequelize) => {
-    // return queryInterface.bulkDelete("User", null, {});
+  down: (queryInterface) => {
+    return queryInterface.bulkDelete("User", null, {});
   }
 };
