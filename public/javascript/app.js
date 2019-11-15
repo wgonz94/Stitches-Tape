e8fa0365c410df450e4bc12b01f21aab9c713508
SirHexxus <jamesmichaelstacy@gmail.com>
2019-11-15 15:47:14 -0800

    modified app.js so that links stop disappearing

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,20 +6,20 @@ $(document).ready(() => {
   $(".modal").modal();
 });
 
-$("#signup").on("click", e => {
+/* $("#signup").on("click", e => {
   e.preventDefault();
   $(".logged-in").show();
   $(".logged-out").hide();
-});
+}); */
 
-$("#signin").on("click", e => {
+/* $("#signin").on("click", e => {
   e.preventDefault();
   $(".logged-in").show();
   $(".logged-out").hide();
-});
+}); */
 
-$("#logout").on("click", e => {
+/* $("#logout").on("click", e => {
   e.preventDefault();
   $(".logged-in").hide();
   $(".logged-out").show();
-});
+}); */
