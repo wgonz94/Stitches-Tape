665382a401d620131ad74d3d4649fb6928d886a4
Scottie <avscott@pacbell.net>
2020-01-08 02:03:21 -0800

    update prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.JWT_SECRET
+    mongoURI: "mongodb+srv://avscott:2k9zAWGm8GMAMZxX@cluster0-njptm.mongodb.net/test?retryWrites=true&w=majority"
+    // mongoURI: process.env.MONGO_URI,
+    // secretOrKey: process.env.JWT_SECRET
   };
