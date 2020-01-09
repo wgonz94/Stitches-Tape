61583983f67df0c33eabdfc9ec83a5e0ce8acb37
William Gonzales <wgonz94@yahoo.com>
2020-01-08 21:52:09 -0800

    reconfigured user.js to work with mongoose

--- models/user.js
+++ models/user.js
@@ -1,46 +1,24 @@
-module.exports = (sequelize, DataTypes) => {
-  const bcrypt = require("bcrypt");
-  const User = sequelize.define(
-    "User",
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        isEmail: true
-      },
-      wantsUpdates: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      }
-    },
-    {}
-  );
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs"); 
 
-  User.beforeCreate((user, options) => {
-    const salt = bcrypt.genSaltSync();
-    user.password = bcrypt.hashSync(user.password, salt);
-  });
-    
-   
-  User.prototype.validPassword = function(password) {
-          return bcrypt.compareSync(password, this.password);
-        }; 
-        
-  User.associate = function(models) {
-    User.hasMany(models.Measurement, {
-      onDelete: "cascade"
-    });
-  };
+const UserSchema = new Schema({
+  firstName: { type: String, default: '', required: true },
+  lastName: { type: String, required: true },
+  email: {type: String, required: true },
+  password: {type: String, required: true },
+  isDeleted: {type: Boolean, default: false },
+  date: { type: Date, default: Date.now }
+});
 
-  return User;
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
+
+UserSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
