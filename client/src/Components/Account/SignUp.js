1fd21b77ffdfec9f59f73fd13526c817c5f17b78
William Gonzales <wgonz94@yahoo.com>
2020-01-13 14:50:21 -0800

    sign up shows 200 code for signup and login, but not able to confirm if data being sent to database

--- client/src/Components/Account/SignUp.js
+++ client/src/Components/Account/SignUp.js
@@ -1,117 +1,269 @@
 import React, { Component } from "react";
+import 'whatwg-fetch'
 
+import { getFromStorage, setInStorage } from './../../utils/storage';
 
-const initialState = {
-  username: "",
-  usernameInvalid: '',
-  email: "",
-  emailInvalid: '',
-  password: "",
-  passwordInvalid: '',
-  passCheck: "",
-  passCheckInvalid: '',
-  moreInfo: false
-};
+    export default class Login extends Component {
+      constructor(props) {
+        super(props);
 
+        this.state = {
+          isLoading: true,
+          isLoggedIn: '',
+          signUpError: '', 
+          signInError: '',
+          logInEmail: "",
+          logInPassword: "",
+          signUpFirstName: "",
+          signUpLastName: "",
+          signUpEmail: "",
+          signUpPassword: ""
+        };
 
-class SignUp extends Component {
-  state = initialState;
+        this.onChangeLogInEmail = this.onChangeLogInEmail.bind(this)
+        this.onChangeLogInPassword = this.onChangeLogInPassword.bind(this)
+        this.onChangeSignUpEmail = this.onChangeSignUpEmail.bind(this)
+        this.onChangeSignUpPassword = this.onChangeSignUpPassword.bind(this)
+        this.onChangeSignUpFirstName = this.onChangeSignUpFirstName.bind(this)
+        this.onChangeSignUpLastName = this.onChangeSignUpLastName.bind(this)
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]:
-        e.target.type === "checkbox" ? e.target.checked : e.target.value
-    });
-    console.log(this.state)
-    // eslint-disable-next-line no-unused-expressions
-    this.state.username.length < 6 ? this.setState({ usernameInvalid: 'Username must be at least 6 characters' }) : this.setState({ usernameInvalid: null })
-    this.state.email.length < 6 ? this.setState({ emailInvalid: 'Email must be at least 6 characters' }) : this.setState({ emailInvalid: null })
-    this.state.password.length < 10 ? this.setState({ passwordInvalid: 'Email must be at least 10 characters' }) : this.setState({ passwordInvalid: null })
-    this.state.password === this.state.passCheck ? this.setState({passCheckInvalid: null }) : this.setState({passCheckInvalid: 'Passwords do not match, please try again'})
-  }
-  
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log(this.state)
-    this.setState(initialState);
-  };
+        this.onLogin = this.onLogin.bind(this);
+        this.onSignUp = this.onSignUp.bind(this);
+      }
 
-  render() {
-    return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <ul className="register-form">
-            <h2>New Account:</h2>
-            <li>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Username"
-                value={this.state.username}
-                onChange={this.handleChange}
-                required
-              />
-              <div className="clear red-text strong">{this.state.usernameInvalid}</div>
-            </li>
-            <li>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="example@email.com"
-                value={this.state.email}
-                onChange={this.handleChange}
-                required
-              />
-              <div className="clear red-text">{this.state.emailInvalid}</div>
-            </li>
-            <li>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                required
-              />
-              <div className="clear red-text">{this.state.passwordInvalid}</div>
-            </li>
-            <li>
-              <input
-                type="password"
-                name="passCheck"
-                id="passCheck"
-                placeholder="Password (Again)"
-                value={this.state.passCheck}
-                onChange={this.handleChange}
-                required
-              />
-              <div className="clear red-text">{this.state.passCheckInvalid}</div>
-            </li>
-            <p>
-              <label>
-                <input
-                  type="checkbox"
-                  id="moreInfo"
-                  value={this.state.moreInfo}
-                  checked={this.state.moreInfo}
-                  onChange={e => this.handleChange(e, true)}
-                />
-                <span>
-                  Please inform me of upcoming Changes, Promotions, and News
-                </span>
-              </label>
-            </p>
+      componentDidMount() {
+        const obj = getFromStorage('the_main_app');
+        if (obj && obj.token){
+          const { token } = obj
+          //verify token
+          fetch('/api/account/verify?token' + token)
+          .then(res => res.json())
+          .then(json => {
+            if(json.success) {
+              this.setState({
+              token,
+              isLoading: false
+            });
+            } else {
+              this.setState({
+                isLoading: false,
+              });
+            }
+          })
+        } else {
+          this.setState({
+            isLoading: false,
+          })
+        }
+      }
+
+      onChangeLogInEmail(event) {
+        this.setState({
+          logInEmail: event.target.value,
+        })
+      }
+      onChangeLogInPassword(event) {
+        this.setState({
+          logInPassword: event.target.value,
+        })
+      }
+      onChangeSignUpEmail(event) {
+        this.setState({
+          signUpEmail: event.target.value,
+        })
+      }
+      onChangeSignUpPassword(event) {
+        this.setState({
+          signUpPassword: event.target.value,
+        })
+      }
+      onChangeSignUpFirstName(event) {
+        this.setState({
+          signUpFirstName: event.target.value,
+        })
+      }
+      onChangeSignUpLastName(event) {
+        this.setState({
+          signUpLastName: event.target.value,
+        })
+      }
+
+      onLogin() {
+        //Grab State
+        const {
+          logInEmail,
+          logInPassword,
+        }= this.state;
+
+        this.setState({
+          isLoading: true,
+        })
+        //Post request to backend
+        fetch('/api/account/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify({
+            email: logInEmail,
+            password: logInPassword,
+          }),
+        }).then(res => res.json())
+          .then(json => {
+            if(json.success){
+              setInStorage('the_main_app', { token: json.token });
+              this.setState({
+                signInError: json.message,
+                isLoading: false,
+                logInEmail: '',
+                logInPassword: '',
+                token: json.token,
+              });
+            }else {
+              this.setState({
+                signInError: json.message,
+                isLoading: false,
+              });
+            }
+          });
+      }
+      onSignUp(){
+        //Grab State
+        const {
+          signUpFirstName,
+          signUpLastName,
+          signUpEmail,
+          signUpPassword
+        }= this.state;
+
+        this.setState({
+          isLoading: true,
+        })
+        console.log(signUpFirstName)
+        console.log(signUpLastName)
+        console.log(signUpEmail)
+        console.log(signUpPassword)
+       
+
+        //Post request to backend
+        fetch("/api/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify({
+            firstName: signUpFirstName,
+            lastName: signUpLastName,
+            email: signUpEmail,
+            password: signUpPassword,
+          }
+          ),
+        }).then(res => res.json())
+          .then(json => {
+            console.log("json", json)
+            if(json.success){
+              this.setState({
+                isLoading: false,
+                signUpEmail: "",
+                signUpPassword: "",
+                signUpFirstName: "",
+                signUpLastName: "",
+              });
+            }else {
+              this.setState({
+                signUpError: json.message,
+                isLoading: false,
+              });
+            }
+          }
+          ).catch(error => {
+            throw(error);
+        });
+      }
+      validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+      }
+
+      handleChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+      }
+
+      handleSubmit = event => {
+        event.preventDefault();
+      }
+      
+      
+      render() {
+        const {
+          isLoading,
+          token,
+          signInError,
+          logInEmail,
+          logInPassword,
+          signUpFirstName,
+          signUpLastName,
+          signUpEmail,
+          signUpPassword,
+          signUpError
+        } = this.state;
+
+        if(isLoading) {
+          return (<div><p>Loading...</p></div>);
+        }
+
+        if(!token) {
+          return (
+          <div>
+            <div>
+
+              {
+                (signInError) ? (
+                  <p>{signInError}</p>
+                ) : (null)
+              }
+                <p>Log In</p>
+                <input type="email" placeholder="Email" value={logInEmail} onChange={this.onChangeLogInEmail}/>
+                <input type="password" placeholder="Password" value={logInPassword} onChange={this.onChangeLogInPassword}/>
+                <button onClick={this.onLogin}>Log In</button>
+            </div>
+            <br />            
             <br />
-            <input type="submit" defaultValue="Create Account" />
-            <div className="clear"> </div>
-          </ul>
-        </form>
-      </div>
-    );
-  }
-}
+            <div>
+            {
+                (signUpError) ? (
+                  <p>{signUpError}</p>
+                ) : (null)
+              }
+                <p>Sign Up</p>
+                <input type="text" placeholder="First Name" value={this.signUpFirstName} onChange={this.onChangeSignUpFirstName}/>
+                <input type="text" placeholder="Last Name" value={this.signUpLastName} onChange={this.onChangeSignUpLastName}/>
+                <input type="email" placeholder="Email" value={this.signUpEmail} onChange={this.onChangeSignUpEmail}/>
+                <input type="password" placeholder="Password" value={this.signUpPassword} onChange={this.onChangeSignUpPassword}/>
+                <button onClick={this.onSignUp}>Sign Up</button>
+            </div>
 
-export default SignUp;
+          </div>)
+        }
+        return (
+          <div>
+      <p>Account: {logInEmail} </p>
+          {/* <TextInput
+          email
+          error="Wrong Email sir"
+          label="Email"
+          success="Great"
+          validate
+        />
+        <TextInput
+          label="Password"
+          password
+        /> */}
+        
+        </div>
+        );
+      }
+    }
