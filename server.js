636822a4c5422143aee94bb7a4a096425d1d7b42
Scottie <avscott@pacbell.net>
2020-01-13 21:28:20 -0800

    removed cookies from server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require('path');
-var cookieParser = require('cookie-parser');
-// var session = require('express-session');
 var morgan = require('morgan');
-// var User = require('./models/User');
 const app = express();
 const mongoose = require('mongoose');
 
@@ -15,28 +12,6 @@ app.use(express.json());
 
 // set morgan to log info about requests for development use
 app.use(morgan('dev'));
-
-// initialize cookie-parser to allow access the cookies stored in the browser
-app.use(cookieParser());
-
-// // initialize express-session to allow tracking for the logged-in user across sessions
-// app.use(session({
-//     key: 'user_sid',
-//     secret: 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         expires: 600000
-//     }
-// }));
-// will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-  if (req.cookies.user_sid && !req.session.user) {
-      res.clearCookie('user_sid');        
-  }
-  next();
-});
 
 // config MongoDB
 const db = require('./config/keys_prod').mongoURI;
@@ -58,8 +33,6 @@ mongoose.connect(db, configOptions)
 app.use('/api/users', require('./routes/api/user'));
 app.use('/api/measurements', require('./routes/api/measurement'));
 app.use('/api/login', require('./routes/login'));
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
 
 // Serve static assets if we're in production
 if(process.env.NODE_ENV === 'production') {
@@ -72,98 +45,6 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-
-// // middleware function to check for logged-in users
-// var sessionChecker = (req, res, next) => {
-//   if (req.session.user && req.cookies.user_sid) {
-  
-//       res.redirect('/dashboard');
-//   } else {
-//       next();
-//   }    
-// };
-
-
-// // route for Home-Page
-// app.get('/', sessionChecker, (req, res) => {
-//   res.redirect('/login');
-// });
-
-
-// // route for user signup
-// app.route('/signup')
-//   .get((req, res) => {
-//       res.sendFile(__dirname + '/public/signup.html');
-//       // res.render('signup', hbsContent);
-//   })
-//   .post((req, res) => {
-//       User.create({
-//           username: req.body.username,
-//           email: req.body.email,
-//           password: req.body.password,
-//           wantsUpdates: req.body.wantsUpdates
-//       })
-//       .then(user => {
-//           req.session.user = user.dataValues;
-//           res.redirect('/account');
-//       })
-//       .catch(error => {
-//           res.redirect('/login');
-//       });
-//   });
-
-
-// // route for user Login
-// app.route('/login')
-//   .get(sessionChecker, (req, res) => {
-//       res.sendFile(__dirname + '/public/login.html');
-//       // res.render('login', hbsContent);
-//   })
-//   .post((req, res) => {
-//       var username = req.body.username,
-//           password = req.body.password;
-
-//       User.findOne({ where: { username: username } }).then(function (user) {
-//           if (!user) {
-//               res.redirect('/login');
-//           } else if (!user.validPassword(password)) {
-//               res.redirect('/login');
-//           } else {
-//               req.session.user = user.dataValues;
-//               res.redirect('/account');
-//           }
-//       });
-//   });
-
-
-// // route for user's dashboard
-// app.get('/account', (req, res) => {
-//   if (req.session.user && req.cookies.user_sid) {
-//   // hbsContent.loggedin = true; 
-//   // hbsContent.userName = req.session.user.username; 
-//   //console.log(JSON.stringify(req.session.user)); 
-//   console.log(req.session.user.username); 
-//   // hbsContent.title = "You are logged in"; 
-//       res.sendFile(__dirname + '/public/account.html');
-//       // res.render('index', hbsContent);
-//   } else {
-//       res.redirect('/login');
-//   }
-// });
-
-
-// // route for user logout
-// app.get('/logout', (req, res) => {
-//   if (req.session.user && req.cookies.user_sid) {
-//   // hbsContent.loggedin = false; 
-//   // hbsContent.title = "You are logged out!"; 
-//       res.clearCookie('user_sid');
-//   // console.log(JSON.stringify(hbsContent)); 
-//       res.redirect('/');
-//   } else {
-//       res.redirect('/login');
-//   }
-// });
 
 // Starting the server, syncing our models ------------------------------------/
 app.listen(PORT, () => {
